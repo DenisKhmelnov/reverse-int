e725dcaa06948e86923f8a61e7c9371a4edf4b42
DenisKhmelnov <denishmelnov@gmail.com>
2021-08-23 23:34:52 +0500

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    let reversedString = Math.abs(n).toString();
+    let output = Array.from(reversedString).reverse().toString().split(',').join('');
+    return output;
 }
